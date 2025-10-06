c2537f885e62accf6a964d8c07ff2387da2db546
Hrishikesh <hrishikesh@aliff.in>
2025-10-06 23:02:05 +0530

    middleware issue resolved

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,13 +1,6 @@
 // Export all middleware functions for easy importing
 // All middleware includes JWT token verification and Bearer token checking
-export { authMiddleware } from './authMiddleware.js';
-export { adminMiddleware } from './adminMiddleware.js';
-export { superAdminMiddleware } from './superAdminMiddleware.js';
-export { recruiterMiddleware } from './recruiterMiddleware.js';
-export { talentMiddleware } from './talentMiddleware.js';
-export { roleMiddleware } from './roleMiddleware.js';
-
-// Default exports
+// Re-export default exports under named aliases
 export { default as authMiddleware } from './authMiddleware.js';
 export { default as adminMiddleware } from './adminMiddleware.js';
 export { default as superAdminMiddleware } from './superAdminMiddleware.js';
